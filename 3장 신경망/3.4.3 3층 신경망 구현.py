c8853084df8443c3d6d7f3680617732eba8d215c
zangzoo <jangjang0022@gmail.com>
2024-09-17 23:15:17 +0900

    3장 신경망

--- 3장 신경망/3.4.3 3층 신경망 구현.py
+++ 3장 신경망/3.4.3 3층 신경망 구현.py
@@ -7,7 +7,7 @@ def identity_function(x):
     return x
 
 def init_work():
-    network={}
+    network={} 
     network['W1']=np.array([[0.1,0.3,0.5], [0.2,0.4,0.6]])
     network['b1']=np.array([0.1,0.2,0.3])
     network['W2']=np.array([[0.1,0.4],[0.2,0.5],[0.3,0.6]])
