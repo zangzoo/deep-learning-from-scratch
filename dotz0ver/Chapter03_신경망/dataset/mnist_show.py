bf690dc5683bc5d3632fce757cf3d6287b0f57c4
dotz0ver <opir0oui@gmail.com>
2024-09-18 08:39:47 +0000

    Upload Chapter03

---
+++ dotz0ver/Chapter03_신경망/dataset/mnist_show.py
@@ -0,0 +1,23 @@
+# MNIST 이미지를 화면으로 표시
+
+import sys, os
+sys.path.append(os.pardir) # 부모 디렉터리의 파일을 가져올 수 있도록 설정
+import numpy as np
+from dataset.mnist import load_mnist
+from PIL import Image
+
+def img_show(img):
+    pil_img = Image.fromarray(np.uint8(img))
+    pil_img.show
+
+(x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
+
+img = x_train[0]
+label = t_train[0]
+print(label) # 5
+
+print(img.shape)
+img = img.reshape(28, 28) # 원래 이미지의 모양으로 변형
+print(img.shape)
+
+img_show(img)
